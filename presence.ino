c35a0927e2ccc6ad3acdda90d42ed37f35a923eb
Mitchell <mitchell@johnson.fyi>
2024-07-18 21:37:19 +0000

    Fix untested code

--- presence.ino
+++ presence.ino
@@ -1,62 +1,34 @@
-// Define implementation-specific variables
-#if defined(ESP32)
-  #ifdef ESP_IDF_VERSION_MAJOR // IDF 4+
-    #if CONFIG_IDF_TARGET_ESP32 // ESP32/PICO-D4
-      constexpr auto MONITOR_SERIAL = Serial;
-      constexpr auto RADAR_SERIAL = Serial1;
-      constexpr int RADAR_RX_PIN = 32;
-      constexpr int RADAR_TX_PIN = 33;
-    #elif CONFIG_IDF_TARGET_ESP32S2
-      constexpr auto MONITOR_SERIAL = Serial;
-      constexpr auto RADAR_SERIAL = Serial1;
-      constexpr int RADAR_RX_PIN = 9;
-      constexpr int RADAR_TX_PIN = 8;
-    #elif CONFIG_IDF_TARGET_ESP32C3
-      constexpr auto MONITOR_SERIAL = Serial;
-      constexpr auto RADAR_SERIAL = Serial1;
-      constexpr int RADAR_RX_PIN = 4;
-      constexpr int RADAR_TX_PIN = 5;
-    #else 
-      #error Target CONFIG_IDF_TARGET is not supported
-    #endif
-  #else // ESP32 Before IDF 4.0
-    constexpr auto MONITOR_SERIAL = Serial;
-    constexpr auto RADAR_SERIAL = Serial1;
-    constexpr int RADAR_RX_PIN = 32;
-    constexpr int RADAR_TX_PIN = 33;
-  #endif
-#elif defined(__AVR_ATmega32U4__)
-  constexpr auto MONITOR_SERIAL = Serial;
-  constexpr auto RADAR_SERIAL = Serial1;
-  constexpr int RADAR_RX_PIN = 0;
-  constexpr int RADAR_TX_PIN = 1;
-#endif
+//Note this is a cut down version of the config from https://github.com/Wovyn/esp32-ld2410-radar-sensor-mqtt-ota/blob/main/src/esp32-ld2410-radar-sensor-mqtt-ota
+//For other devices you might need a different config
+#define MONITOR_SERIAL Serial
+#define RADAR_SERIAL Serial1
+#define RADAR_RX_PIN 32
+#define RADAR_TX_PIN 33
 
-// WiFi and MQTT configuration
-constexpr char ssid[] = "<WIFI SSID>";
-constexpr char password[] = "<WIFI Password>";
-constexpr char mqtt_server[] = "192.168.1.32";
-constexpr char CLIENT_ID[] = "office_presence";
-constexpr char DEVICE_NAME[] = "Office Presence";
-constexpr char WillTopic[] = "$CONNECTED/" CLIENT_ID;
-String stateTopic = "home/office_presence/state";
-
-int stillThreshold = 30;
-int movingThreshold = 30;
-
-// Include necessary libraries
 #include <ld2410.h>
 #include <WiFi.h>
 #include <MQTT.h>
 #include <ArduinoJson.h>
 
-// Initialize radar and flags
+#define CLIENT_ID "office_presence"
+#define DEVICE_NAME "Office Presence"
+
 ld2410 radar;
 bool engineeringMode = false;
 String command;
 
 uint32_t lastReading = 0;
 bool radarConnected = false;
+
+constexpr char ssid[] = "Johnson";
+constexpr char password[] = "aaaaaaaa";
+constexpr char mqtt_server[] = "192.168.1.32";
+constexpr char WillTopic[] = "$CONNECTED/" CLIENT_ID;
+String stateTopic = "home/office_presence/state";
+
+int stillThreshold = 30;
+int movingThreshold = 30;
+bool logReads = false;
 
 // Initializing WiFi and MQTT client
 WiFiClient net;
@@ -218,7 +190,7 @@ void publishSensorData() {
 
   //binary sensor to indicate if someone is present or not
   String presence = "OFF";
-  if(radar.movingTargetEnergy() > movingThreshold || radar.stationaryTargetEnergy() > movingThreshold){
+  if(radar.presenceDetected()){
     presence = "ON";
   }
 
@@ -227,43 +199,293 @@ void publishSensorData() {
 
   String payload;
   serializeJson(stateDoc, payload);
-  Serial.println(F("publishing: "));
-  Serial.print(payload.c_str());
+  if(logReads){
+    Serial.println(F("publishing: "));
+    Serial.println(payload.c_str());
+  }
   client.publish(stateTopic.c_str(), payload.c_str());
 }
 
 void setup(void)
 {
   MONITOR_SERIAL.begin(19200); //Feedback over Serial Monitor
-  delay(5000); //5 second delay before startup
-  pinMode(LED_BUILTIN, OUTPUT);
-  WiFi.begin(ssid, password);
+  delay(500); //Give a while for Serial Monitor to wake up
+  setupWiFi();
   client.begin(mqtt_server, net);
   client.onMessage(mqttCallback);
-  connectMqtt();
-  setupWiFi();
-  radar.begin(RADAR_SERIAL, RADAR_RX_PIN, RADAR_TX_PIN);
-  MONITOR_SERIAL.println("setting up device");
+  //radar.debug(Serial); //Uncomment to show debug information from the library on the Serial Monitor. By default this does not show sensor reads as they are very frequent.
 
-  client.subscribe("/hello");
+  RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN); //UART for monitoring the radar
+
+  delay(500);
+  MONITOR_SERIAL.print(F("\nConnect LD2410 radar TX to GPIO:"));
+  MONITOR_SERIAL.println(RADAR_RX_PIN);
+  MONITOR_SERIAL.print(F("Connect LD2410 radar RX to GPIO:"));
+  MONITOR_SERIAL.println(RADAR_TX_PIN);
+  MONITOR_SERIAL.print(F("LD2410 radar sensor initialising: "));
+  if(radar.begin(RADAR_SERIAL))
+  {
+    MONITOR_SERIAL.println(F("OK"));
+    MONITOR_SERIAL.print(F("LD2410 firmware version: "));
+    MONITOR_SERIAL.print(radar.firmware_major_version);
+    MONITOR_SERIAL.print('.');
+    MONITOR_SERIAL.print(radar.firmware_minor_version);
+    MONITOR_SERIAL.print('.');
+    MONITOR_SERIAL.println(radar.firmware_bugfix_version, HEX);
+  }
+  else
+  {
+    MONITOR_SERIAL.println(F("not connected"));
+  }
+  MONITOR_SERIAL.println(F("Supported commands\nread: read current values from the sensor\nreadconfig: read the configuration from the sensor\nsetmaxvalues <motion gate> <stationary gate> <inactivitytimer>\nsetsensitivity <gate> <motionsensitivity> <stationarysensitivity>\nenableengineeringmode: enable engineering mode\ndisableengineeringmode: disable engineering mode\nrestart: restart the sensor\nreadversion: read firmware version\nfactoryreset: factory reset the sensor\n"));
 }
 
-void loop(void)
+void loop()
 {
-  // Maintain WiFi connection
-  if (WiFi.status() != WL_CONNECTED) {
-    setupWiFi();
+  radar.read(); //Always read frames from the sensor
+  if(MONITOR_SERIAL.available())
+  {
+    char typedCharacter = MONITOR_SERIAL.read();
+    if(typedCharacter == '\r' || typedCharacter == '\n')
+    {
+      command.trim();
+      if(command.equals("logReads"))
+      {
+        command = "";
+        logReads = !logReads;
+      } else if(command.equals("read"))
+      {
+        command = "";
+        MONITOR_SERIAL.print(F("Reading from sensor: "));
+        if(radar.isConnected())
+        {
+          MONITOR_SERIAL.println(F("OK"));
+          if(radar.presenceDetected())
+          {
+            if(radar.stationaryTargetDetected())
+            {
+              MONITOR_SERIAL.print(F("Stationary target: "));
+              MONITOR_SERIAL.print(radar.stationaryTargetDistance());
+              MONITOR_SERIAL.print(F("cm energy: "));
+              MONITOR_SERIAL.println(radar.stationaryTargetEnergy());
+            }
+            if(radar.movingTargetDetected())
+            {
+              MONITOR_SERIAL.print(F("Moving target: "));
+              MONITOR_SERIAL.print(radar.movingTargetDistance());
+              MONITOR_SERIAL.print(F("cm energy: "));
+              MONITOR_SERIAL.println(radar.movingTargetEnergy());
+            }
+          }
+          else
+          {
+            MONITOR_SERIAL.println(F("nothing detected"));
+          }
+        }
+        else
+        {
+          MONITOR_SERIAL.println(F("failed to read"));
+        }
+      }
+      else if(command.equals("readconfig"))
+      {
+        command = "";
+        MONITOR_SERIAL.print(F("Reading configuration from sensor: "));
+        if(radar.requestCurrentConfiguration())
+        {
+          MONITOR_SERIAL.println(F("OK"));
+          MONITOR_SERIAL.print(F("Maximum gate ID: "));
+          MONITOR_SERIAL.println(radar.max_gate);
+          MONITOR_SERIAL.print(F("Maximum gate for moving targets: "));
+          MONITOR_SERIAL.println(radar.max_moving_gate);
+          MONITOR_SERIAL.print(F("Maximum gate for stationary targets: "));
+          MONITOR_SERIAL.println(radar.max_stationary_gate);
+          MONITOR_SERIAL.print(F("Idle time for targets: "));
+          MONITOR_SERIAL.println(radar.sensor_idle_time);
+          MONITOR_SERIAL.println(F("Gate sensitivity"));
+          for(uint8_t gate = 0; gate <= radar.max_gate; gate++)
+          {
+            MONITOR_SERIAL.print(F("Gate "));
+            MONITOR_SERIAL.print(gate);
+            MONITOR_SERIAL.print(F(" moving targets: "));
+            MONITOR_SERIAL.print(radar.motion_sensitivity[gate]);
+            MONITOR_SERIAL.print(F(" stationary targets: "));
+            MONITOR_SERIAL.println(radar.stationary_sensitivity[gate]);
+          }
+        }
+        else
+        {
+          MONITOR_SERIAL.println(F("Failed"));
+        }
+      }
+      else if(command.startsWith("setmaxvalues"))
+      {
+        uint8_t firstSpace = command.indexOf(' ');
+        uint8_t secondSpace = command.indexOf(' ',firstSpace + 1);
+        uint8_t thirdSpace = command.indexOf(' ',secondSpace + 1);
+        uint8_t newMovingMaxDistance = (command.substring(firstSpace,secondSpace)).toInt();
+        uint8_t newStationaryMaxDistance = (command.substring(secondSpace,thirdSpace)).toInt();
+        uint16_t inactivityTimer = (command.substring(thirdSpace,command.length())).toInt();
+        if(newMovingMaxDistance > 0 && newStationaryMaxDistance > 0 && newMovingMaxDistance <= 8 && newStationaryMaxDistance <= 8)
+        {
+          MONITOR_SERIAL.print(F("Setting max values to gate "));
+          MONITOR_SERIAL.print(newMovingMaxDistance);
+          MONITOR_SERIAL.print(F(" moving targets, gate "));
+          MONITOR_SERIAL.print(newStationaryMaxDistance);
+          MONITOR_SERIAL.print(F(" stationary targets, "));
+          MONITOR_SERIAL.print(inactivityTimer);
+          MONITOR_SERIAL.print(F("s inactivity timer: "));
+          command = "";
+          if(radar.setMaxValues(newMovingMaxDistance, newStationaryMaxDistance, inactivityTimer))
+          {
+            MONITOR_SERIAL.println(F("OK, now restart to apply settings"));
+          }
+          else
+          {
+            MONITOR_SERIAL.println(F("failed"));
+          }
+        }
+        else
+        {
+          MONITOR_SERIAL.print(F("Can't set distances to "));
+          MONITOR_SERIAL.print(newMovingMaxDistance);
+          MONITOR_SERIAL.print(F(" moving "));
+          MONITOR_SERIAL.print(newStationaryMaxDistance);
+          MONITOR_SERIAL.println(F(" stationary, try again"));
+          command = "";
+        }
+      }
+      else if(command.startsWith("setsensitivity"))
+      {
+        uint8_t firstSpace = command.indexOf(' ');
+        uint8_t secondSpace = command.indexOf(' ',firstSpace + 1);
+        uint8_t thirdSpace = command.indexOf(' ',secondSpace + 1);
+        uint8_t gate = (command.substring(firstSpace,secondSpace)).toInt();
+        uint8_t motionSensitivity = (command.substring(secondSpace,thirdSpace)).toInt();
+        uint8_t stationarySensitivity = (command.substring(thirdSpace,command.length())).toInt();
+        if(motionSensitivity >= 0 && stationarySensitivity >= 0 && motionSensitivity <= 100 && stationarySensitivity <= 100)
+        {
+          MONITOR_SERIAL.print(F("Setting gate "));
+          MONITOR_SERIAL.print(gate);
+          MONITOR_SERIAL.print(F(" motion sensitivity to "));
+          MONITOR_SERIAL.print(motionSensitivity);
+          MONITOR_SERIAL.print(F(" & stationary sensitivity to "));
+          MONITOR_SERIAL.print(stationarySensitivity);
+          MONITOR_SERIAL.println(F(": "));
+          command = "";
+          if(radar.setGateSensitivityThreshold(gate, motionSensitivity, stationarySensitivity))
+          {
+            MONITOR_SERIAL.println(F("OK, now restart to apply settings"));
+          }
+          else
+          {
+            MONITOR_SERIAL.println(F("failed"));
+          }
+        }
+        else
+        {
+          MONITOR_SERIAL.print(F("Can't set gate "));
+          MONITOR_SERIAL.print(gate);
+          MONITOR_SERIAL.print(F(" motion sensitivity to "));
+          MONITOR_SERIAL.print(motionSensitivity);
+          MONITOR_SERIAL.print(F(" & stationary sensitivity to "));
+          MONITOR_SERIAL.print(stationarySensitivity);
+          MONITOR_SERIAL.println(F(", try again"));
+          command = "";
+        }
+      }
+      else if(command.equals("enableengineeringmode"))
+      {
+        command = "";
+        MONITOR_SERIAL.print(F("Enabling engineering mode: "));
+        if(radar.requestStartEngineeringMode())
+        {
+          MONITOR_SERIAL.println(F("OK"));
+        }
+        else
+        {
+          MONITOR_SERIAL.println(F("failed"));
+        }
+      }
+      else if(command.equals("disableengineeringmode"))
+      {
+        command = "";
+        MONITOR_SERIAL.print(F("Disabling engineering mode: "));
+        if(radar.requestEndEngineeringMode())
+        {
+          MONITOR_SERIAL.println(F("OK"));
+        }
+        else
+        {
+          MONITOR_SERIAL.println(F("failed"));
+        }
+      }
+      else if(command.equals("restart"))
+      {
+        command = "";
+        MONITOR_SERIAL.print(F("Restarting sensor: "));
+        if(radar.requestRestart())
+        {
+          MONITOR_SERIAL.println(F("OK"));
+        }
+        else
+        {
+          MONITOR_SERIAL.println(F("failed"));
+        }
+      }
+      else if(command.equals("readversion"))
+      {
+        command = "";
+        MONITOR_SERIAL.print(F("Requesting firmware version: "));
+        if(radar.requestFirmwareVersion())
+        {
+          MONITOR_SERIAL.print('v');
+          MONITOR_SERIAL.print(radar.firmware_major_version);
+          MONITOR_SERIAL.print('.');
+          MONITOR_SERIAL.print(radar.firmware_minor_version);
+          MONITOR_SERIAL.print('.');
+          MONITOR_SERIAL.println(radar.firmware_bugfix_version,HEX);
+        }
+        else
+        {
+          MONITOR_SERIAL.println(F("Failed"));
+        }
+      }
+      else if(command.equals("factoryreset"))
+      {
+        command = "";
+        MONITOR_SERIAL.print(F("Factory resetting sensor: "));
+        if(radar.requestFactoryReset())
+        {
+          MONITOR_SERIAL.println(F("OK, now restart sensor to take effect"));
+        }
+        else
+        {
+          MONITOR_SERIAL.println(F("failed"));
+        }
+      }
+      else
+      {
+        MONITOR_SERIAL.print(F("Unknown command: "));
+        MONITOR_SERIAL.println(command);
+        command = "";
+      }
+    }
+    else
+    {
+      command += typedCharacter;
+    }
   }
 
-  // Maintain MQTT connection
+  if(radar.isConnected() && millis() - lastReading > 1000)  //Report every 1000ms
+  {
+    lastReading = millis();
+    publishSensorData();
+
   if (!client.connected()) {
     connectMqtt();
   }
+  
   client.loop();
-
-  // Publish sensor data at regular intervals
-  if (millis() - lastReading >= 5000) { // Change the interval as needed
-    publishSensorData();
-    lastReading = millis();
-  }
+  
 }
